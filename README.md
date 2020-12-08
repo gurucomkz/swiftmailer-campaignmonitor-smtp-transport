6f7ff3f4dfab8f80c59da504b53cfa95b2509b9b
gurucomkz <sergey.aegis@gmail.com>
2020-12-08 19:30:22 +1300

    first commit

---
+++ README.md
@@ -0,0 +1,56 @@
+#Mailgun SMTP Transport for Swiftmailer
+
+## Install
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```bash
+$ php composer.phar require --prefer-dist gurucomkz/campaignmonitor-smtp-transport "dev-master"
+```
+
+or add
+
+```json
+"gurucomkz/campaignmonitor-smtp-transport": "dev-master"
+```
+
+to the require section of your `composer.json` file.
+
+
+## Usage
+
+Once the extension is installed, simply use it in your code:
+
+### In configuration file ###
+
+```php
+'components' => [
+    'mailer' => [
+        'class' => '\yii\swiftmailer\MailerMailer',
+        'transport' => [
+            'class' => '\gurucomkz\campaignmonitor\SmtpTransport',
+            'mailgunDomain' => '<Domain Name>',
+            'privateApiKey' => '<Private API Key>',
+        ],
+    ],
+```
+
+### Sending images to Johny ###
+
+```php
+\Yii::$app->mailer
+    ->compose()
+    ->setFrom('me@nomail.com')
+    ->setTo('john.doe@nomail.com')
+    ->setSubject('Message to Johny')
+    ->setTextBody('Hello, Johny! Take a look at our party pics :)')
+    ->attach('/image-1.jpeg')
+    ->attach('/image-2.jpeg')
+    ->send();
+```
+
+## License
+
+The BSD License (BSD). Please see [License File](LICENSE.md) for more information.
---
+++ src/SmtpTransport.php
@@ -0,0 +1,167 @@
+<?php
+
+
+namespace Gurucomkz\campaignmonitor;
+
+use CS_REST_Transactional_ClassicEmail;
+use Swift_Transport;
+use Swift_Mime_Message;
+use Swift_Events_EventListener;
+use Swift_TransportException;
+
+
+class SmtpTransport implements Swift_Transport
+{
+
+    protected $_clientID = null;
+    protected $_apiKey = null;
+
+    /** Connection status */
+    protected $_started = true;
+
+    /**
+     * Test if this Transport mechanism has started.
+     *
+     * @return bool
+     */
+    public function isStarted()
+    {
+        return $this->_started;
+    }
+
+    /**
+     * Start this Transport mechanism.
+     */
+    public function start()
+    {
+    }
+
+    /**
+     * Stop this Transport mechanism.
+     */
+    public function stop()
+    {
+    }
+
+    public function setApiKey($value)
+    {
+        $this->_apiKey = $value;
+    }
+
+    public function getApiKey()
+    {
+        return $this->_apiKey;
+    }
+
+    public function setClientID($value)
+    {
+        $this->_clientID = $value;
+    }
+
+    public function getClientID()
+    {
+        return $this->_clientID;
+    }
+
+
+    /**
+     * Send the given Message.
+     *
+     * Recipient/sender data will be retrieved from the Message API.
+     * The return value is the number of recipients who were accepted for delivery.
+     *
+     * @param Swift_Mime_Message $message
+     * @param string[]  $failedRecipients An array of failures by-reference
+     *
+     * @throws Swift_TransportException
+     * @return int sent messages count
+     */
+    public function send(Swift_Mime_Message $message, &$failedRecipients = null)
+    {
+        if (null === $message->getHeaders()->get('To')) {
+            throw new Swift_TransportException(
+                'Cannot send message without a recipient'
+            );
+        }
+
+        $auth = [
+            'auth' => $this->getApiKey(),
+        ];
+        $client_id = $this->getClientID();
+
+        $mgClient = new CS_REST_Transactional_ClassicEmail($auth, $client_id);
+        $result = $mgClient->send( $this->getPostData($message), null );
+
+        return $result->http_status_code == 200 ? 1 : 0;
+    }
+
+    /**
+     * Register a plugin in the Transport.
+     *
+     * @param \Swift_Events_EventListener $plugin
+     */
+    public function registerPlugin(Swift_Events_EventListener $plugin)
+    {
+    }
+
+    /**
+     * Looks at the message headers to find post data.
+     *
+     * @param Swift_Mime_Message $message
+     *
+     * @return array $postData
+     */
+    protected function getPostData(Swift_Mime_Message $message)
+    {
+        // get "form", "to" etc..
+        $postData = [
+            'Subject' => $message->getSubject(),
+            'From' => $message->getFrom(),
+            'ReplyTo' => $message->getReplyTo(),
+            'To' => $message->getTo(),
+            'CC' => $message->getCc(),
+            'Html' => $message->toString(),
+            'ConsentToTrack' => 'No',
+        ];
+
+        return $postData;
+    }
+
+    /**
+     * @param Swift_Mime_Message $message
+     *
+     * @return array
+     */
+    protected function prepareRecipients(Swift_Mime_Message $message)
+    {
+        $headerNames = ['from', 'to', 'bcc', 'cc'];
+        $messageHeaders = $message->getHeaders();
+        $postData = [];
+
+        foreach ($headerNames as $name) {
+            /** @var \Swift_Mime_Headers_MailboxHeader $h */
+            $h = $messageHeaders->get($name);
+            $postData[$name] = $h === null ? [] : $h->getAddresses();
+        }
+
+        // Merge 'bcc' and 'cc' into 'to'.
+        $postData['to'] = array_merge($postData['to'], $postData['bcc'], $postData['cc']);
+        unset($postData['bcc']);
+        unset($postData['cc']);
+
+        // Remove Bcc to make sure it is hidden
+        $messageHeaders->removeAll('bcc');
+
+        return $postData;
+    }
+
+    /**
+     * Get the special o:* headers. https://documentation.mailgun.com/api-sending.html#sending.
+     *
+     * @return array
+     */
+    public static function getMailgunHeaders()
+    {
+        return ['o:tag', 'o:campaign', 'o:deliverytime', 'o:dkim', 'o:testmode', 'o:tracking', 'o:tracking-clicks', 'o:tracking-opens'];
+    }
+}
